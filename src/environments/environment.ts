2f149fa9feaa79e87789b9b4d3f198ae102be536
DanBothWolrds <dan@bothworlds.co.nz>
2025-03-11 10:00:28 +1300

    wip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://wb-app-dev.azurewebsites.net/api',
+  apiBaseUrl: 'https://wb-app-dev.azurewebsites.net/',
   apiKey: 'ign.PjoxCjTMRVkrbXZqfPKLxFUfJuSyJnkRKqxiUWRtQHFjFzWbomzflAgScsDLGlsj',
-  apiUrl: 'https://wb-app-dev.azurewebsites.net/api',
+  apiUrl: 'https://wb-app-dev.azurewebsites.net/',
   stripe: {
     publishableKey: 'pk_test_51GuRE3LWubzAXyqFnQKJ7uIUu4aOJTo8hlWrGxuqn50o94uYK5fa6T0ChUE38zikbyDl3DyDUMdcOwjZpRwr9vWW000EW736hz',
   }
